08290ff69365192d740be367c35cb88d998bd05b
lamarckf <lamarck.psy@hotmail.com>
2021-08-12 19:22:01 -0300

    add database

---
+++ qt6/src/janelalogin.cpp
@@ -0,0 +1,15 @@
+#include "janelalogin.h"
+#include "ui_janelalogin.h"
+
+JanelaLogin::JanelaLogin(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::JanelaLogin)
+{
+    ui->setupUi(this);
+}
+
+JanelaLogin::~JanelaLogin()
+{
+    delete ui;
+}
+
---
+++ qt6/src/janelalogin.h
@@ -0,0 +1,21 @@
+#ifndef JANELALOGIN_H
+#define JANELALOGIN_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class JanelaLogin; }
+QT_END_NAMESPACE
+
+class JanelaLogin : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    JanelaLogin(QWidget *parent = nullptr);
+    ~JanelaLogin();
+
+private:
+    Ui::JanelaLogin *ui;
+};
+#endif // JANELALOGIN_H
